1bd64229ce9331d3af7d63674fb1cf5b77c5e84e
Matt Royer <matthew.royer@gmail.com>
2024-02-11 19:07:44 -0700

    Stable version

---
+++ app.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import discord
+import os
+from dotenv import load_dotenv
+from llm_wrapper import LLMWrapper
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+
+intents = discord.Intents.default()
+intents.messages = True
+intents.guilds = True
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+MESSAGES = [{'role': 'system', 'content': 'You are a helpful assistant.'}]
+
+@client.event
+async def on_ready():
+  print(f'Logged on as {client.user}')
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+     return
+
+  async with message.channel.typing():
+    try:
+      reply = await ask_guppi(message.content)
+      if reply:
+        await message.channel.send(reply)
+    except Exception as e:
+      await message.channel.send(e)
+
+client.run(TOKEN)
+
+async def ask_guppi(message):
+  llm = LLMWrapper('openai', 'gpt-3.5-turbo')
+  # llm = LLMWrapper('ollama', 'dolphin2.2-mistral')
+
+  MESSAGES.append({'role': 'user', 'content': message})
+
+  reply = ''
+
+  for response in llm.send_message(MESSAGES):
+    if response['role'] == 'assistant' and response['content'] != None:
+      reply += response['content']
+
+    if response['done']:
+      MESSAGES.append({'role': 'assistant', 'content': reply})
+      break
+  return reply
---
+++ llm_wrapper.py
@@ -0,0 +1,39 @@
+class LLMWrapper:
+  def __init__(self, llm_type, model_name):
+    self.llm_type = llm_type
+    self.model_name = model_name
+    self.llm_client = self._initialize_llm()
+
+  def _initialize_llm(self):
+    if self.llm_type == 'openai':
+      from openai import OpenAI
+      import os
+      api_key = os.getenv('OPENAI_API_KEY')
+      return OpenAI(api_key=api_key)
+    elif self.llm_type == 'ollama':
+      import ollama
+      return ollama
+    else:
+      raise ValueError("Unsupported LLM type")
+
+  def send_message(self, messages, stream=True):
+    if self.llm_type == 'openai':
+      completion = self.llm_client.chat.completions.create(model=self.model_name, messages=messages, stream=stream)
+      for chunk in completion:
+        choice = chunk.choices[0].delta
+        content = choice.content
+        role = 'assistant'
+        done = chunk.choices[0].finish_reason == 'stop'
+        yield {
+          'content': content,
+          'role': role,
+          'done': done
+        }
+    elif self.llm_type == 'ollama':
+      stream = self.llm_client.chat(model=self.model_name, messages=messages, stream=stream)
+      for chunk in stream:
+        yield {
+          'content': chunk['message']['content'],
+          'role': chunk['message']['role'],
+          'done': chunk.get('done', False)
+        }
